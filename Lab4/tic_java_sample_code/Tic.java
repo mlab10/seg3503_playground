da1278b2581f76dffc735f0e3d6d4e443975a6ae
mlab10 <myriamlekol2028@gmail.com>
2026-06-25 07:21:59 -0400

    boardsEqual pass2

--- Lab4/tic_java_sample_code/Tic.java
+++ Lab4/tic_java_sample_code/Tic.java
@@ -4,36 +4,59 @@ public class Tic {
 	String[][] board;
 	int rows;
 	int cols;
-	String turn;
+	String turnX;
+	String turnO;
 
 	//Constructor
 	public Tic(int row, int col) {
 		board = new String[row][col];
 		rows = row;
 		cols = col;
-		turn = "X";
+		turnO = "O";
+		turnX = "X";
 		for(int i = 0; i<row; i++) {
 			for(int j = 0; j<col; j++) {
 				board[i][j] = "_";
 			}
 		}
 	}
-	
-	public boolean boardsEqual(String[][] board1, String[][] board2) {
 
-		if(board1.length != board2.length || board1[0].length != board2[0].length) {
+	public boolean boardsizeEquals(String[][] board2) {
+		if(board.length != board2.length || board[0].length != board2[0].length) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Method to check if two boards are equal
+	 * @param board2 the second board to compare with
+	 * @return true if the boards are equal, false otherwise
+	 */
+	
+	public boolean boardsEqual(String[][] board2) {
+
+		if(!boardsizeEquals(board2)) {
 			return false;
 		}
 
 		for(int i = 0; i < rows; i++) {
 			for(int j = 0; j < cols; j++) {
-				if(!board1[i][j].equals(board2[i][j])) {
+				if(!board[i][j].equals(board2[i][j])) {
 					return false;
 				}
 			}
 		}
 		return true;
 	}
+
+	public boolean cellsEqual(String[][] board2, int row, int col) {
+		if(board[row][col].equals(board2[row][col])) {
+			return true;
+		}
+		return false;
+	}
+
 
 
 }
--- Lab4/tic_java_sample_code/TicTest.java
+++ Lab4/tic_java_sample_code/TicTest.java
@@ -6,13 +6,10 @@ import org.junit.jupiter.api.Test;
 
 class TicTest {
 
-	/**
-	 * Test if the boardsEqual method correctly identifies two boards as equal or not equal.
-	 */
 	@Test
 	void boardsEqualTest() {
 		Tic board = new Tic(4, 4);
 		Tic board2 = new Tic(3, 5);
-		assertFalse(board.boardsEqual(board.board, board2.board));
+		assertFalse(board.boardsEqual(board2.board));
 	}
 }
